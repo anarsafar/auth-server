1c381ac33c38d24e43e48174e7b83bb054fb5f9d
Anar Safarov <anarsferov@gmail.com>
2023-09-16 15:04:19 +0400

    fix: redirect url

--- build/swagger-config.js
+++ build/swagger-config.js
@@ -1,11 +1,9 @@
-'use strict';
-var __importDefault =
-    (this && this.__importDefault) ||
-    function (mod) {
-        return mod && mod.__esModule ? mod : { default: mod };
-    };
-Object.defineProperty(exports, '__esModule', { value: true });
-const swagger_jsdoc_1 = __importDefault(require('swagger-jsdoc'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
 const swaggerOptions = {
     definition: {
         openapi: '3.0.0',
@@ -15,7 +13,7 @@ const swaggerOptions = {
             description: 'API documentation for Auth Server'
         }
     },
-    apis: ['src/swagger/**/*.js']
+    apis: ['src/swagger/**/*.ts']
 };
 const swaggerSpec = (0, swagger_jsdoc_1.default)(swaggerOptions);
 exports.default = swaggerSpec;
