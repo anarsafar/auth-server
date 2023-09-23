73703d00e943d7698afb8dcfe9166f86c0b5ab90
Anar Safarov <anarsferov@gmail.com>
2023-09-23 20:33:30 +0400

    fix: image upload validation

--- build/src/schemas/update/updateUserSchema .js
+++ build/src/schemas/update/updateUserSchema .js
@@ -9,7 +9,7 @@ const updateUserSchema = joi_1.default.object({
     name: joi_1.default.string().min(3).required(),
     bio: joi_1.default.string().max(150).required(),
     phone: joi_1.default.string().min(6).max(20).required(),
-    image: joi_1.default.object()
+    image: joi_1.default.alternatives().try(joi_1.default.object(), joi_1.default.string())
 });
 const validateUpdateUserSchema = (0, validateSchema_1.default)(updateUserSchema);
 exports.default = validateUpdateUserSchema;
--- src/schemas/update/updateUserSchema .ts
+++ src/schemas/update/updateUserSchema .ts
@@ -6,7 +6,7 @@ const updateUserSchema = Joi.object({
     name: Joi.string().min(3).required(),
     bio: Joi.string().max(150).required(),
     phone: Joi.string().min(6).max(20).required(),
-    image: Joi.object()
+    image: Joi.alternatives().try(Joi.object(), Joi.string())
 });
 
 const validateUpdateUserSchema = validateSchema(updateUserSchema);
